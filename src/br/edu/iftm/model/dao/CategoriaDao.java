408cae3f9459dc8aeea7b4b64a4c28a3eed98ba2
Desenvolvedor <Desenvolvedor@DESKTOP-KNR35R1>
2016-09-02 20:34:21 -0300

    Versão Aula 02 do 09

---
+++ src/br/edu/iftm/model/dao/CategoriaDao.java
@@ -0,0 +1,40 @@
+package br.edu.iftm.model.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import br.edu.iftm.model.domain.Categoria;
+
+public class CategoriaDao {
+	
+	@PersistenceContext(unitName="ds2016-2Ecommerce")
+	private EntityManager entityManager;
+
+	@Transactional
+	public void salvar(Categoria categoria){
+		Categoria merge = entityManager.merge(categoria);
+		entityManager.persist(merge);
+	}
+	
+	@Transactional
+	public void atualizar(Categoria categoria) {
+		Categoria merge = entityManager.merge(categoria);
+		entityManager.persist(merge);
+	}
+
+	@Transactional
+	public void excluir(Categoria categoria) {
+		Categoria merge = entityManager.merge(categoria);
+		entityManager.remove(merge);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Categoria> buscar(Categoria categoria) {
+		Query query = entityManager.createQuery("from Categoria");
+		return query.getResultList();
+	}
+}
